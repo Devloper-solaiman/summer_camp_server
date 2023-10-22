196ab8f0cdf06207d26da8ba0251cdfa4200bab8
Devloper-solaiman <devlopersolaiman@gmail.com>
2023-10-22 17:05:04 +0600

    probLem

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+const App = () => {
+  return (
+    <div>
+   <h1 className="text-7xl text-center mt-10 text-red-600 font-bold underline">
+      Hello world!
+    </h1>
+    <button className="btn btn-outline btn-secondary">Secondary</button>
+
+        </div>
+  );
+};
+
+export default App;
